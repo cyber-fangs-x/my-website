a571700ab91936e0fdc7b9da9a0d76e61a2621b7
cyber-fangs-x <cyber.fangs.x@proton.me>
2026-09-09 12:55:30 -0400

    Set up automatic Vite deploy to GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/my-website/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL('./index.html', import.meta.url).pathname,
+        about: new URL('./about.html', import.meta.url).pathname,
+        contact: new URL('./contact.html', import.meta.url).pathname,
+        projects: new URL('./projects.html', import.meta.url).pathname,
+        project1: new URL('./projects/project-1.html', import.meta.url).pathname,
+      },
+    },
+  },
+})
